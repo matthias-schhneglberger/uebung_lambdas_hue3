82f64dfd69f82738dd4814032d5750c552f554ce
Matthias Schneglberger <matthias.schneglberger@gmx.at>
2019-10-02 15:27:48 +0200

    added Printable interface

---
+++ src/uebung1/Printable.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uebung1;
+
+import java.util.List;
+
+/**
+ *
+ * @author matth
+ */
+public interface Printable {
+    void print(List<Weapon> weapons);
+}
