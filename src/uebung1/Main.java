c1b14c87934a57e8ee467b3f6dfb8c9619e26af7
Matthias Schneglberger <matthias.schneglberger@gmx.at>
2019-10-02 14:21:59 +0200

    add test (succesfully)

--- src/uebung1/Main.java
+++ src/uebung1/Main.java
@@ -5,17 +5,35 @@
  */
 package uebung1;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
 /**
  *
  * @author matth
  */
 public class Main {
-
+    public static List<Weapon> weaponsList;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        
+        ReadCSV csvReader = new ReadCSV();
+        
+        csvReader.read();
+        
+        weaponsList = csvReader.getWeapons();
+        
+        Comparator<Weapon> sortByDamageIncrease = 
+            (Weapon w1, Weapon w2) -> w1.getDamage() - w2.getDamage();
+        
+        
+        weaponsList.sort(sortByDamageIncrease);
+        
     }
     
 }
---
+++ test/uebung1/MainTest.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uebung1;
+
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author matth
+ */
+public class MainTest {
+    
+    public MainTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class Main.
+     */
+//    @Test
+//    public void testMain() {
+//        System.out.println("main");
+//        String[] args = null;
+//        Main.main(args);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+    
+    @Test
+    public void testSortByDamageIncrease(){
+        System.out.println("Testing sort Weapons by damage increase");
+        
+        Main.main(null);
+        List<Weapon> weapons = Main.weaponsList;
+        boolean richtig = true;
+        for(int i = weapons.size()-1; i > 0; i--){
+            if(!  (weapons.get(i).getDamage() >= weapons.get(i-1).getDamage())  ){
+                richtig = false;
+            }
+        }
+        
+        assertTrue("nicht Richtig sortiert", richtig);
+        
+    }
+    
+}
